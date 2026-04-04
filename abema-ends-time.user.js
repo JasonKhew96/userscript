5145f76bec41ed91add048ea32aca79c811d6f9c
Js0n <contact@jasonkhew96.dev>
2026-04-04 19:58:00 +0800

    Update abema-ends-time.user.js

--- abema-ends-time.user.js
+++ abema-ends-time.user.js
@@ -2,7 +2,7 @@
 // @name        Abema premium/free ends time
 // @match       https://abema.tv/*
 // @grant       none
-// @version     1.7
+// @version     1.8
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @author      JasonKhew96
 // @description shows abema premium/free ends time
@@ -10,39 +10,37 @@
 // ==/UserScript==
 
 ;(() => {
-  const dateTimeformatter = new Intl.DateTimeFormat("ja-JP", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
+  const dateTimeformatter = new Intl.DateTimeFormat('ja-JP', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
     hour12: false,
-    timeZone: "Asia/Tokyo",
+    timeZone: 'Asia/Tokyo'
   })
   const whitelist = [
-    "com-content-list-ContentListEpisodeItem__overview",
-    "com-content-list-ContentListItemList",
-    "c-application-DesktopAppContainer__main",
-    "com-content-list-ContentListItemList",
+    'com-contentlist-ContentlistContainer',
+    'com-contentlist-ContentlistSectionList'
   ]
 
   const episodeMap = new Map()
   /** @type {globalThis.Window} */
   const win =
-    typeof unsafeWindow === "object"
+    typeof unsafeWindow === 'object'
       ? unsafeWindow
-      : typeof window === "object"
+      : typeof window === 'object'
       ? window
       : this
-  ;((Response) => {
+  ;(Response => {
     Response.prototype.__json = Response.prototype.json
     Response.prototype.json = function () {
       /** @type {globalThis.Response} */
       const __this = this
       /** @type {Promise<any>} */
       let jsonPromise = __this.__json.apply(__this, arguments)
-      jsonPromise = jsonPromise.then((res) => {
-        if ("episodeGroupContents" in res) {
+      jsonPromise = jsonPromise.then(res => {
+        if ('episodeGroupContents' in res) {
           for (const content of res.episodeGroupContents) {
             episodeMap.set(content.id, content)
           }
@@ -64,31 +62,27 @@
         const target = mutation.target
         const addedNodes = mutation.addedNodes
         if (!addedNodes.length) continue
-        if (
-          !whitelist.includes(target.classList[0]) &&
-          target.tagName != "MAIN"
-        )
+        if (!whitelist.includes(target.classList[0]) || target.tagName != 'DIV')
           continue
-        const episodes = target.className.includes(
-          "com-content-list-ContentListEpisodeItem__overview"
+
+        const episodes = target.querySelectorAll(
+          '.com-contentlist-SectionItemList__item'
         )
-          ? [target]
-          : target.querySelectorAll(
-              ".com-content-list-ContentListEpisodeItem__overview"
-            )
         for (const episode of episodes) {
-          const url = new URL(
-            episode.querySelector(
-              ".com-content-list-ContentListEpisodeItem__link"
-            ).href
-          )
-          const id = url.pathname.split("/").pop()
+          const id =
+            episode[
+              Object.keys(episode).filter(key =>
+                key.startsWith('__reactFiber')
+              )[0]
+            ].key
           const contents = episodeMap.get(id)
           const term = contents.video.terms[0]
           const endAt = dateTimeformatter.format(new Date(term.endAt * 1000))
-          const onDemandType = term.onDemandType == 1 ? "プレミアム" : "無料"
-          const span = episode.querySelector(".com-vod-VODLabel__text")
-          span.textContent = onDemandType + " ～ " + endAt
+          const onDemandType = term.onDemandType == 1 ? 'プレミアム' : '無料'
+          const span = episode.querySelector(
+            '.com-shared-viewing_type-ViewingTypeLabel__text'
+          )
+          span.textContent = onDemandType + ' ～ ' + endAt
         }
       }
     }
