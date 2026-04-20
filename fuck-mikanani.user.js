4b4acb375d795d16e2de31f55590598b545e9cd8
Js0n <contact@jasonkhew96.dev>
2026-04-20 11:43:59 +0800

    feat(mikanani): fix mobile ui without changing to desktop mode

--- fuck-mikanani.user.js
+++ fuck-mikanani.user.js
@@ -2,19 +2,33 @@
 // @name        Fuck mikanani
 // @match       https://mikanani.me/*
 // @grant       none
-// @version     1.1
+// @version     2.0
 // @author      JasonKhew96
-// @description disable mikan mobile ui
+// @description fix mikan mobile ui
 // @downloadURL https://github.com/JasonKhew96/userscript/raw/refs/heads/master/fuck-mikanani.user.js
 // ==/UserScript==
 
 (()=>{
-	for (e of document.getElementsByClassName("hidden-lg")) {
-		e.style.display = "none"
-	}
-	for (e of document.getElementsByClassName("hidden-xs")) {
-		e.classList.remove("hidden-xs")
-		e.classList.remove("hidden-sm")
-	}
-	new Blazy
+  const addClassic = () => {
+     const a = document.createElement("a")
+    a.classList.add("link")
+    a.href = "/Home/Classic"
+    a.textContent = "列表"
+    const li = document.createElement("li")
+    li.classList.add("m-tool-search-change")
+    li.append(a)
+    const mToolListUl = document.querySelector(".m-tool-list > ul")
+    const mToolSearchChange = document.querySelector(".m-tool-search-change[onclick]")
+    mToolListUl.insertBefore(li, mToolSearchChange)
+  }
+
+  const fixClassic = () => {
+    const skContainer = document.getElementById("sk-container")
+    if (!skContainer) return
+    skContainer.style.width = "100%"
+    skContainer.style.paddingTop = "3.9rem"
+  }
+
+  addClassic()
+  fixClassic()
 })()
